af02e15a6680ede4c8cd599f070ccd3b7c8782c0
Adam Przybyłowski <adam.przybylowski@wp.pl>
2019-12-28 16:13:37 +0100

    ex1

---
+++ src/ExeChap1/Ex2.java
@@ -0,0 +1,15 @@
+package ExeChap1;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 10) {
+            x = x + 1;
+            if (x > 3) {
+                System.out.println("x = " + x);
+            }
+
+        }
+    }
+
+}

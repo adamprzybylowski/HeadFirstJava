8ed6e0db8892afedeb01f61b123004d44fb8a57e
Adam Przybyłowski <adam.przybylowski@wp.pl>
2020-01-02 12:33:07 +0100

    Islands test Array

---
+++ src/Island/IslandTestArray.java
@@ -0,0 +1,26 @@
+package Island;
+
+public class IslandTestArray {
+    public static void main(String[] args) {
+        int[] index = new int[4];
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+
+        String[] islands = new String[4];
+        islands[0] = "Bermud";
+        islands[1] = "Fiji";
+        islands[2] = "Azory";
+        islands[3] = "Kozumel";
+
+        int y = 0;
+        int ref;
+        while (y < 4) {
+            ref = index[y];
+            System.out.print("island :  ");
+            System.out.println(islands[ref]);
+            y = y + 1;
+        }
+    }
+}
